2bccdfafc165ebf9b1fe0b0a763d0f5ada7fdfa2
“arrahman” <arrahman1003@gmail.com>
2023-04-02 16:28:09 +0600

    datatype.py

---
+++ datatype.py
@@ -0,0 +1,37 @@
+
+a= "Hello World"
+print(a)
+print(type(a))
+#b = 20
+#print(b)
+#c = 20.5
+#print(c)
+#d = 1j
+#print(d)
+#e = ["apple", "banana", "cherry"]
+#print(e)
+#f = ("apple", "banana", "cherry")
+#print(f)
+#g = range(6)
+#print(g)
+#h = {"name" : "John", "age" : 36}
+#print(h)
+#i = {"apple", "banana", "cherry"}
+#print(i)
+#j = frozenset({"apple", "banana", "cherry"})
+#print(j)
+#k = True
+#print(k)
+#l = b"Hello"
+#print(l)
+#m = bytearray(5)
+#print(m)
+#n = memoryview(bytes(5))
+#print(n)
+#o = None
+#print(o)
+#p= int(20)
+#print(p)
+#x1 = float(20.5)
+#print(
+#srm-rnwn-zkb
